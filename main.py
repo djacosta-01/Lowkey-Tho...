051d81e715fce59dd0dbfc45205f5ec32aafb616
Maya Epps <mayaepps@Mayas-MacBook-Pro.local>
2019-08-10 23:51:59 -0700

    Created app.yaml and main.py and two templates

---
+++ main.py
@@ -0,0 +1,24 @@
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_env.get_template('templates/main.html')
+        self.response.write(template.render())
+
+class GamePage(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_env.get_template('templates/game.html')
+        self.response.write(template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/game', GamePage),
+], debug=True)
