16c8a7c557799a132259ebaecc875db0c3c6e783
yarrow isaacs <isaacsyarrow@gmail.com>
2019-08-12 12:06:42 -0700

    sdjasdiajdijidajdiasasd

--- cards.py
+++ cards.py
@@ -1,5 +1,11 @@
-cards = []
+import random *
+
+
+cards = ['Low-key though...','']
 
 
 def make_card(promt):
-    cards.append(promt)
+    cards.append(promt
+
+def get_card():
+    cards[random.randint(0,len(cards))]
