4ba3ac21e63207412f2e15164383c4097dc10ff3
Maya Epps <mayaepps@Mayas-MacBook-Pro.local>
2019-08-13 10:59:06 -0700

    finished game page timer

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// the timer
-   var time_in_minutes = 2;
+   var time_in_minutes = 1;
    var current_time = Date.parse(new Date());
    var deadline = new Date(current_time + time_in_minutes*60*1000);
    function time_remaining(endtime){
@@ -16,20 +15,28 @@
        var clock = document.getElementById(timer);
        function update_clock(){
            var t = time_remaining(endtime);
-           if (t.minutes < 10) { //time < 10:00
+
+           if (t.minutes < 10 && t.seconds < 10) {
+               clock.textContent = '0' + t.minutes + ':' + '0' + t.seconds;
+               if (t.minutes < 1) {
+                 clock.style.color="red";
+               }
+           }
+           else if (t.minutes < 10) {
                clock.textContent = '0' + t.minutes + ':' + t.seconds;
            }
-           else if (t.minutes < 10 && t.seconds < 10) {
-               clock.textContent = '0' + t.minutes + ':' + '0' + t.seconds;
+           else if (t.seconds < 10) {
+               clock.textContent = t.minutes + ':' + '0' + t.seconds;
            }
-           else { // time = 10:00
+           else {
                clock.textContent = t.minutes + ':' + '0' + t.seconds;
            }
            if (t.total<=0) {
              clearInterval(timeinterval);
+             window.location.pathname = '/results';
            }
        }
-   update_clock(); // run function once at first to avoid delay
+   update_clock();
    var timeinterval = setInterval(update_clock,1000);
    }
    run_clock("clockdiv", deadline);
