86f3eb1c9eca5ab53a0a58f34ef3cff7c7d7d51f
Diego Acosta <djacosta@D-i-e-g-os-MacBook-Air.local>
2019-08-13 15:56:01 -0700

    added animations to the results page

---
+++ scripts/results.js
@@ -0,0 +1,10 @@
+const vote = document.querySelectorAll(".card");
+
+for (let i = 0; i < vote.length; i++){
+  //add eventListener to each element with a card class
+  vote[i].addEventListener('click', function(){
+    vote[i].style = '20px';
+    vote[i].style.transform = 'rotate(360deg)';
+    vote[i].style.transition = '3s all';
+  });
+}
