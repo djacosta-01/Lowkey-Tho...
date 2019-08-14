67000851851d3558c49c3152e73a9b1f3098e046
Maya Epps <mayaepps@Mayas-MacBook-Pro.local>
2019-08-14 15:39:57 -0700

    put vote buttons on the results page cards

--- scripts/results.js
+++ scripts/results.js
@@ -2,9 +2,12 @@ const vote = document.querySelectorAll(".card");
 
 for (let i = 0; i < vote.length; i++){
   //add eventListener to each element with a card class
+
   vote[i].addEventListener('click', function(){
     vote[i].style = '20px';
     vote[i].style.transform = 'rotate(360deg)';
     vote[i].style.transition = '1s all';
+    console.log(vote[i].childNodes[1])
+    vote[i].childNodes[1].src = "/images/votebutton.png"
   });
 }
