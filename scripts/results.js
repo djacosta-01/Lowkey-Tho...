af522cdc6f0c9066cd507212b15f917e8fe5e001
Diego Acosta <djacosta.07@gmail.com>
2019-08-14 17:47:08 -0700

    FINALLY MADE THE GREEN BUTTON TO STAY AND THE GREY BUTTON TO DISAPPEAR WHEN NOT HOVERED OVER

--- scripts/results.js
+++ scripts/results.js
@@ -1,8 +1,23 @@
 const vote = document.querySelectorAll(".card");
 let canVote = true;
-
+let button = document.querySelector('#grey-button');
 for (let i = 0; i < vote.length; i++){
   //add eventListener to each element with a card class
+  vote[i].addEventListener('mouseenter', function(){
+
+    button = vote[i].childNodes[1];
+    console.log(button)
+    button.style.opacity = '1';
+  })
+  vote[i].addEventListener('mouseleave', function(){
+
+    button = vote[i].childNodes[1];
+    console.log(button.src)
+    if(button.src != "http://localhost:8080/images/votebutton.png"){
+      button.style.opacity = '0';
+    }
+
+  })
 
   vote[i].addEventListener('click', function(){
     vote[i].style = '20px';
@@ -12,6 +27,5 @@ for (let i = 0; i < vote.length; i++){
       vote[i].childNodes[1].src = "/images/votebutton.png"
       canVote = false;
     }
-
   });
 }
