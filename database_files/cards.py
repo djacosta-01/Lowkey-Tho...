6f6e0ad1fe7ba9338894b42742854ee929ad58f5
Maya Epps <mayaepps@Mayas-MacBook-Pro.local>
2019-08-14 17:49:32 -0700

    made image check appear

--- database_files/cards.py
+++ database_files/cards.py
@@ -7,7 +7,7 @@ def make_card(promt):
 def get_card():
     cards = ['Low-key though','The song that is the low-keyiest of bangers is ',
     'Low-key though when I was a kid', 'Low-key tho, the thing about pets is',
-    'My mother was low-key right when she said','Low-key if I did not waste my time on YouTube',
+    'My mother was low-key right when she said',"Low-key if I didn't waste my time on YouTube",
     'The lair is low-key', 'Low-key tho, I really want to','I low-key hate it when',
     'Low-key tho, why does everyone think that', 'Low-key tho, I was this many years old when I realized',
     'I lowkey hate it when', 'I lowkey love how','I thought by now I would have',
--- scripts/results.js
+++ scripts/results.js
@@ -1,17 +1,29 @@
+'use strict';
+
 const vote = document.querySelectorAll(".card");
 let canVote = true;
 
 for (let i = 0; i < vote.length; i++){
-  //add eventListener to each element with a card class
-
+  console.log(vote[i]);
+  vote[i].addEventListener('mouseenter', () => greyCheckOn(vote[i]));
+  vote[i].addEventListener('mouseout', () => greyCheckOff(vote[i]));
   vote[i].addEventListener('click', function(){
-    vote[i].style = '20px';
+  vote[i].style = '20px';
     if (canVote === true) {
       vote[i].style.transform = 'rotate(360deg)';
       vote[i].style.transition = '1s all';
       vote[i].childNodes[1].src = "/images/votebutton.png"
       canVote = false;
     }
-
   });
+}
+
+function greyCheckOn(card){
+    card.childNodes[1].style.display="block";
+    console.log(card);
+}
+
+function greyCheckOff(card){
+    card.childNodes[1].style.display="none";
+    console.log(card);
 }
